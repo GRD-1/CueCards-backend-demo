33a266794fee50c0214098e628082270f6515bba
GRD <d.grebelny@gmail.com>
2023-12-20 18:10:31 +0400

    chore: connect typeorm
    connect typeorm to the project with env settings and docker

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,22 +1,13 @@
 import { ConnectionOptions } from 'typeorm';
 import * as process from 'process';
 
-// const config: ConnectionOptions = {
-//   type: 'postgres',
-//   host: process.env.POSTGRES_HOST,
-//   port: process.env.POSTGRES_PORT_EXTERNAL as number | undefined,
-//   username: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DB
-// };
-
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'postgres-db',
-  port: process.env.POSTGRES_PORT as number | undefined || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB || 'tododb',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT_INTERNAL as number | undefined,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   // entities: [`${__dirname}/**/*.entity.ts`, `${__dirname}/**/*.entity.js`],
   migrationsRun: false,
   logging: true,
