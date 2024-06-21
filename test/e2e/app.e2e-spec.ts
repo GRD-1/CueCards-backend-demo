b8048978c08f0472f0a6e31a491e8ba0f4d2be7d
Dmitry <dmitry.grebelny@1win.pro>
2024-06-21 22:01:34 +0400

    build: update a test config schema

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -4,16 +4,16 @@ import request from 'supertest';
 import { AppModule } from '@/app.module';
 
 describe('testing tests', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  // let app: INestApplication;
+  //
+  // beforeEach(async () => {
+  //   const moduleFixture: TestingModule = await Test.createTestingModule({
+  //     imports: [AppModule],
+  //   }).compile();
+  //
+  //   app = moduleFixture.createNestApplication();
+  //   await app.init();
+  // });
 
   it('testing testy test', () => {
     expect(2).toEqual(2);
