9199850e6a074e3a92c7a66c61c590e42889e738
Dmitry <dmitry.grebelny@1win.pro>
2024-07-19 19:10:07 +0400

    refactor: [CC-36] delete unused entities

--- src/modules/card-statistics/card-statistics.entity.ts
+++
@@ -1,8 +0,0 @@
-export class CardStatisticsEntity {
-  cardId: number;
-  authorId: number;
-  fsTotalAnswers: number;
-  fsCorrectAnswers: number;
-  bsTotalAnswers: number;
-  bsCorrectAnswers: number;
-}
