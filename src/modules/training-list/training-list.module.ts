4c433988ae0ec556fb5cdb71656056fa718809b8
Dmitry <dmitry.grebelny@1win.pro>
2024-07-22 23:51:47 +0400

    feat: [CC-64] clean up the project

--- src/modules/training-list/training-list.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrainingListController } from './training-list.controller';
-import { TrainingListService } from './training-list.service';
-
-@Module({
-  controllers: [TrainingListController],
-  providers: [TrainingListService],
-})
-export class TrainingListModule {}
